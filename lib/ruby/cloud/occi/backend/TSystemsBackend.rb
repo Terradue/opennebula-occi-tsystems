032933eabc075f6b1f7842c383edff9d12d88a65
simonetripodi <simone.tripodi@gmail.com>
2011-11-10 14:55:08 +0100

    first checkin of TSystemsBackend.rb - initial draft kindly borrowed from original DummyBackend

---
+++ lib/ruby/cloud/occi/backend/TSystemsBackend.rb
@@ -0,0 +1,80 @@
+##############################################################################
+#  Copyright 2011 Terradue srl
+#  
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#  
+#      http://www.apache.org/licenses/LICENSE-2.0
+#  
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+##############################################################################
+
+require 'occi/ActionDelegator'
+require 'occi/infrastructure/Compute'
+
+module OCCI
+  module Backend
+    class TSystemsBackend
+      
+      def initialize()
+        @computeObjects = []
+        @networkObjects = []
+        @storageObjects = []
+        
+        # Register methods for actions
+        delegator = OCCI::ActionDelegator.instance
+        delegator.register_method_for_action(OCCI::Infrastructure::Compute::ACTION_START, self, :compute_start)
+        delegator.register_method_for_action(OCCI::Infrastructure::Compute::ACTION_STOP,  self, :compute_stop)
+      end     
+      
+      def compute_start(action, parameters, resource)
+        $log.debug("compute_start: action [#{action}] with parameters [#{parameters}] called for resource [#{resource}]!")
+      end
+      
+      def compute_stop(action, parameters, resource)
+        $log.debug("compute_stop: action [#{action}] with parameters [#{parameters}] called for resource [#{resource}]!")
+      end
+      
+      def create_compute_instance(computeObject)
+        @computeObjects << computeObject
+      end
+      
+      def delete_compute_instance(computeObject)
+        @computeObjects.delete(computeObject)
+      end
+      
+      def create_network_instance(networkObject)
+        @networkObjects << networkObject
+      end
+      
+      def delete_network_instance(networkObject)
+        @networkObjects.delete(networkObject)
+      end
+      
+      def create_storage_instance(storageObject)
+        @storageObjects << storageObject
+      end
+      
+      def delete_storage_instance(storageObject)
+        @storageObjects.delete(storageObject)
+      end
+      
+      def get_all_vnet_ids
+        return []
+      end
+      
+      def get_all_image_ids
+        return []
+      end
+      
+      def print_configuration()
+      end
+      
+    end
+  end  
+end
